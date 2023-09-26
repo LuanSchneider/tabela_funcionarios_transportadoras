cdd3e6c16a3a5bf7eeb6e4ccd6d6c234a0e34838
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-09-26 20:10:24 -0300

    atualição

--- transp.js
+++ transp.js
@@ -1,4 +1,4 @@
-function calcularSoma () {
+function calcularSoma() {
   // Obtenha a tabela pelo ID
   var tabela = document.getElementById("tabelaTransporte");
 
@@ -12,9 +12,16 @@ function calcularSoma () {
       soma += valor;
   }
 
+  // Formate a soma para incluir pontos e vírgulas corretamente
+  soma = soma.toFixed(2).toString();
+  soma = soma.replace(/(\d)(\d{8})$/,"$1.$2"); // Coloca o ponto dos milhões
+  soma = soma.replace(/(\d)(\d{5})$/,"$1.$2"); // Coloca o ponto dos milhares
+  soma = soma.replace(/(\d)(\d{2})$/,"$1,$2"); // Coloca a vírgula antes dos 2 últimos dígitos
+
   // Atualize o resultado com a soma total
-  document.getElementById("resultado").textContent = "Soma dos valores: ", soma;toFixed(2);
+  document.getElementById("resultado").textContent = "Soma dos valores: R$" + soma;
 }
+
 for (var i = 0; i < linhas.length; i++) {
   linhas[i].addEventListener('click', function() {
     // Quando uma linha é clicada, adicione um "x" à primeira célula
