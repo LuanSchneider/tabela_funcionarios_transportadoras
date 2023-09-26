1d06ccef6d31688ac299ce592e16c7c1d48bcf27
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-09-26 16:26:18 -0300

    atualização

--- transp.js
+++ transp.js
@@ -1,14 +1,14 @@
 function calcularSoma() {
-    var tabela = document.getElementById("tabelaTransporte");
-    var soma = 0;
+  var tabela = document.getElementById("tabelaTransporte");
+  var soma = 0;
 
-    // Loop através de todas as linhas e soma os valores da coluna especificada
-    for (var i = 0; i < tabela.rows.length; i++) {
-        soma += parseFloat(tabela.rows[i].cells[7].value);
-    }
+  // Loop através de todas as linhas e soma os valores da coluna especificada
+  for (var i = 1; i < tabela.rows.length; i++) {
+      soma += parseFloat(tabela.rows[i].cells[7].innerText);
+  }
 
-    // Exibe o resultado no parágrafo com id "resultado"
-    document.getElementById("resultado").innerText = "Soma dos valores: R$" + soma;
+  // Exibe o resultado no parágrafo com id "resultado"
+  document.getElementById("resultado").innerText = "Soma dos valores: R$" + soma.toFixed(2);
 }
 // Primeiro, obtenha todas as linhas da tabela
 var linhas = document.getElementById('tabelaTransporte').rows;
