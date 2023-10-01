f44e0f8dbbe3d9362da5f48c2769a0e2b937e50c
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-09-30 22:12:08 -0300

    atualização

--- transp.js
+++ transp.js
@@ -1,23 +1,14 @@
-function calcularSoma2(){
-var soma = 0;
-var tabela = document.getElementById("tabelaTransporte"); // Obtém a primeira tabela na página
-console.log(tabela)
-// Loop através das linhas da tabela
-for (var i = 0; i < tabela.rows.length; i++) {
-    var row = tabela.rows[i];
-    for (var j = 0; j < row.cells.length; j++) {
-      var cellValue = parseFloat(row.cells[7] ? row.cells[7].textContent : "0");
-      // Converte o texto da célula em um número
-        if (!isNaN(cellValue)) {
-            soma += cellValue; // Adiciona o valor da célula à soma se for um número
-        }
-    }
+function calcularSomaValor(coluna) {
+  let sum = 0;
+  for (let i = 0; i < coluna.length; i++) {
+    sum += parseFloat(coluna[7].textContent);
+  }
+  return sum;
 }
 
-// Exibe a soma no elemento "resultado"
-var resultadoElement = document.getElementById("resultado");
-resultadoElement.textContent = "Soma dos valores: R$" + soma.toFixed(2); // Arredonda a soma para 2 casas decimais
-
+let coluna = [ "7"];
+let resultado = somarColuna(coluna);
+console.log(resultado); // 15
   for (var i = 0; i < linhas.length; i++) {
     linhas[i].addEventListener('click', function() {
       // Quando uma linha é clicada, adicione um "x" à primeira célula
@@ -25,4 +16,4 @@ resultadoElement.textContent = "Soma dos valores: R$" + soma.toFixed(2); // Arre
     });
   }
   
-}
+
