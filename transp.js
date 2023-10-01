c9f1e8dc1b99ba79c5e5a87a0d6523720931bf02
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-10-01 15:08:24 -0300

    atuakização

--- transp.js
+++ transp.js
@@ -1,7 +1,8 @@
-var tabela = document.getElementById('minhaTabela');
-var total = 0;
+function calcularsomavalor(){
+var tabela = document.getElementById('tabelaTransporte');
 
-for (var i = 1; i < tabela.rows.length; i++) {
+
+for (var i = 2; i < tabela.rows.length; i++) {
     var valorCelula = parseFloat(tabela.rows[i].cells[7].innerHTML);
     total += valorCelula;
 }
@@ -10,9 +11,9 @@ console.log('A soma da coluna é: ' + total);
 
   for (var i = 0; i < linhas.length; i++) {
     linhas[i].addEventListener('click', function() {
-      // Quando uma linha é clicada, adicione um "x" à primeira célula
+     
       this.cells[0].innerText = 'x';
     });
   }
-  
+}
 
